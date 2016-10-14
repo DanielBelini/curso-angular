e27b07cd09a354e1050e1996dbc4afd6ac01181a
DANIELBELINI <danielpuc2013@gmail.com>
2016-10-14 02:15:10 -0300

    testando o angular 2 TypeScipt snippets feito pelo johnpapa

---
+++ projeto-02/app/cursos/checkList.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'selector',
+    templateUrl: 'feature.component.html'
+})
+export class FeatureComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
+
+//estrutura criado pelo angular 2 TypeScript Snippets
